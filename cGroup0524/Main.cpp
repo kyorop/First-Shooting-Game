9f72a0b7d6fc122a57248daa833afb3cee23531e
Kyoro_p <longcorridor@outlook.jp>
2014-06-06 21:58:19 +0900

    敵とプレイヤーの表示と移動

--- cGroup0524/Main.cpp
+++ cGroup0524/Main.cpp
@@ -1,7 +1,13 @@
 #include "DxLib.h"
 
 #define PLAYER_SPEED 3
+#define ENEMY_SPEED 3
 
+enum Direction
+{
+	RIGHT,
+	LEFT,
+};
 
 // WinMainŠÖ”
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
@@ -20,12 +26,14 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 	int handle_enemy = LoadGraph("img/Sikaku.png");
 	int enemy_x;
 	int enemy_y;
+	int enemy_muki;
 
 	player_x = 288;
 	player_y = 0;
 
 	enemy_x = 0;
 	enemy_y = 50;
+	enemy_muki = RIGHT;
 	
 	while (CheckHitKey(KEY_INPUT_ESCAPE) == 0)
 	{
@@ -40,6 +48,23 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 		if (CheckHitKey(KEY_INPUT_RIGHT)) 
 			player_x += PLAYER_SPEED;
 
+		if (enemy_muki == RIGHT)
+			enemy_x += ENEMY_SPEED;
+		else if (enemy_muki == LEFT)
+			enemy_x -= ENEMY_SPEED;
+
+		if (enemy_x > 576)
+		{
+			enemy_x = 576;
+			enemy_muki = LEFT;
+		}
+		
+		if (enemy_x < 0)
+		{
+			enemy_x = 0;
+			enemy_muki = RIGHT;
+		}
+
 		if (player_x < 0) 
 			player_x = 0;
 		if (player_x > 640 - 64)
@@ -51,6 +76,7 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 
 		// ƒ{[ƒ‹ŒN‚ğ•`‰æ
 		DrawGraph(player_x, player_y, handle_player, FALSE);
+		DrawGraph(enemy_x, enemy_y, handle_enemy, FALSE);
 
 		ScreenFlip();
 		if (ProcessMessage() == -1)
